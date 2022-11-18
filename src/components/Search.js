baf51b10ada7cc8a87a0aaab18df9970d369e4bc
Yuval Ofek <YuvalOfek97@gmail.com>
2022-11-18 15:12:28 +0200

    Fetch the api, deleted Books

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route, useNavigate } from "react-router-dom";
-import { Books } from './books';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Search.css";
 
 const Search = (props) => {
     const [query, setQuery] = useState("");
-    console.log(Books[0].items.map(book => (book.volumeInfo.imageLinks)));
+    const [booker, setBooker] = useState([]);
+    const fetchData = () => {
+        return fetch("https://www.googleapis.com/books/v1/volumes?q=search+terms")
+              .then((response) => response.json())
+              .then((data) => setBooker(data.items));
+              
+      }
+      useEffect(() => {
+          fetchData();
+        },[])
     return (
         <div className='position-relative'>
             <p className='mx-3'>
@@ -18,9 +26,9 @@ const Search = (props) => {
             <div className='position-relative w-100'>
                 <div className='position-relative p-5 mb-5'>
                     <input type={"text"} className={"position-absolute text-center w-50 mb-4 py-2 fs-5 search"} placeholder={"Search..."} onChange={e => setQuery(e.target.value)}></input>
-                </div>
+                    </div>
                 <table className='mx-auto w-50'>
-                    {Books[0].items.filter(book => book.volumeInfo.title.toLowerCase().includes(query.toLowerCase())).map(book => (
+                    {booker.filter(book => book.volumeInfo.title.toLowerCase().includes(query.toLowerCase())).map(book => (
                         <tr>
                             <td className='pt-2 '>
                             {book.volumeInfo.imageLinks ? <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} /> : <img src={"https://via.placeholder.com/150"} alt={book.volumeInfo.title} />}
